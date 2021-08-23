da2901945d555512f4f00a18b73a08d36d7ca9ec
Carsmaniac <mickledapickle@me.com>
2021-08-23 16:53:15 +1000

    Create in-game items JSON
    A JSON file has been created listing all of the in-game items involved
    in the Terra Blade crafting tree. A program has also been written to
    convert a CSV file exported from Google Sheets to properly-formatted
    JSON, as the ingredients list formatting was causing issues.

---
+++ csv-json-converter.py
@@ -0,0 +1,40 @@
+# Maintaining huge JSON files kinda sucks, it's easier to keep a list in Google Sheets,
+# export it as CSV and convert that to JSON, formatted just the way I need it.
+
+import csv
+
+csv_list = []
+with open("input.csv") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        csv_list.append(row)
+
+file = open("output.json", "w")
+file.write("{\n")
+file.write("\t\"inGameItems\": [\n")
+for item in csv_list:
+    file.write("\t\t{\n")
+    file.write("\t\t\t\"name\": \"{}\",\n".format(item["name"]))
+    file.write("\t\t\t\"displayName\": \"{}\",\n".format(item["displayName"]))
+    file.write("\t\t\t\"craftingStation\": \"{}\",\n".format(item["craftingStation"]))
+    # This is kind of messy, and the reason why this program exists
+    # Convert "blade-of-grass 1,lights-bane 1" to "[[1, 1],[16, 1]]" for example
+    ingredient_list_1 = item["ingredients"].split(",")
+    ingredient_list_2 = []
+    for raw_ingredient in ingredient_list_1:
+        ingredient_list_2.append(raw_ingredient.split(" "))
+    ingredient_list_3 = []
+    for raw_ingredient in ingredient_list_2:
+        for each_ingredient in csv_list:
+            if each_ingredient["name"] == raw_ingredient[0]:
+                ingredient_list_3.append("[{}, {}]".format(each_ingredient["id"], raw_ingredient[1]))
+    file.write("\t\t\t\"ingredients\": [{}],\n".format(",".join(ingredient_list_3)))
+    file.write("\t\t\t\"acquisition\": \"{}\",\n".format(item["acquisition"]))
+    file.write("\t\t\t\"mod\": \"{}\"\n".format(item["mod"]))
+    if item == csv_list[-1]:
+        file.write("\t\t}\n")
+    else:
+        file.write("\t\t},\n")
+file.write("\t]\n")
+file.write("}\n")
+file.close()
